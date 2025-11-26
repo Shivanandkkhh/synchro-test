e3dde328f1769da35fd9cf49afc6796f2bd1e2f9
System Administrator <root@Shivanands-MacBook-Air.local>
2025-11-26 20:05:16 +0530

    cart validation

--- extensions/cart-checkout-validation/src/cart_validations_generate_run.js
+++ extensions/cart-checkout-validation/src/cart_validations_generate_run.js
@@ -13,7 +13,13 @@ export function cartValidationsGenerateRun(input) {
   console.error("running");
   
   const errors = input.cart.lines
-    .filter(({ quantity }) => quantity > 1)
+    .filter(({ quantity, sellingPlanAllocation }) => {
+      // If cart line has a selling plan, limit quantity to 2
+      if (sellingPlanAllocation) {
+        return quantity > 2;
+      }
+
+    })
     .map(() => ({
       message: "Not possible to order more than one of each",
       target: "$.cart",
